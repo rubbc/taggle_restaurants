05ec7aa3ff3395af0578e47b6f19d97d84b20de9
rubencattan <rubencattan5@gmail.com>
2019-05-20 20:12:51 +0200

    did some shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :restaurants
+
+  get '/tagged', to: "restaurants#tagged", as: :tagged
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
