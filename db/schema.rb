b25ddf6f899c77e61bfef59dfaa4b6242720fe48
rubencattan <rubencattan5@gmail.com>
2019-06-03 19:40:46 +0200

    r

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_185825) do
+ActiveRecord::Schema.define(version: 2019_06_03_172959) do
 
   create_table "arrondissements", force: :cascade do |t|
     t.integer "number"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_185825) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "arrondissement"
+    t.string "address"
   end
 
   create_table "taggings", force: :cascade do |t|
